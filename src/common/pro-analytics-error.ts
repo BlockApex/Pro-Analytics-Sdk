c067c71d554d202e6e8a65fd04dc48a50ab4e233
MANIAHMED <abdulmani11111111@gmail.com>
2024-01-23 18:50:33 +0500

    feat(Pro-analytics-error): implemented a class for catching error

---
+++ src/common/pro-analytics-error.ts
@@ -0,0 +1,10 @@
+export class ProAnalyticError extends Error {
+    error_code?: string
+  
+    constructor(message: string, error_code?: string) {
+      super(message)
+      this.error_code = error_code
+    }
+  }
+  
+  export const logPrefix = 'pro-analytic:'
