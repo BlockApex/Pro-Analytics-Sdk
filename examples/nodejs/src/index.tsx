685ae61184f2974832b72f066b86e2ee5cfd56c9
MANIAHMED <abdulmani11111111@gmail.com>
2024-01-24 12:16:22 +0500

    feat(examples): wip: ts config files added

---
+++ examples/nodejs/src/index.tsx
@@ -0,0 +1,9 @@
+// import { ProAnalyticsApi, SupportedChainId } from 'pro-analytics-sdk'
+
+// See more examples in /examples/cra
+// ;(async function () {
+//   const ProAnalyticsApi = new ProAnalyticsApi({ chainId: SupportedChainId.LIGHTLINK_PHOENIX_MAINNET })
+
+//   const version = await ProAnalyticsApi.getVersion()
+
+// })()
